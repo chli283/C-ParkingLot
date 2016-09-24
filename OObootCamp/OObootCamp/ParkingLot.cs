a6f82da8efa0a27429ddffaddf3be76ee97a07e2
Blink <blink@thoughtworks.com>
2016-09-24 10:51:59 +0800

    parkingLot's park and pick functional feature

---
+++ OObootCamp/OObootCamp/ParkingLot.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OObootCamp
+{
+    public class ParkingLot
+    {
+        public static List<string> lot = new List<string>();
+
+        public int park(string car)
+        {
+           
+           lot.Add(car);
+            return lot.IndexOf(car);
+        }
+
+        public string pick(int ticket)
+        {
+            return lot[ticket];
+
+        }
+    }
+}
---
+++ OObootCamp/TestOObootCamp/TestParkingLot.cs
@@ -0,0 +1,40 @@
+﻿using OObootCamp;
+using Xunit;
+
+namespace TestOObootCamp
+{
+    public class TestParkingLot
+    {
+        [Fact]
+        public void should_be_able_to_pick_same_car_to_tracy_when_tracy_park_car()
+        {
+            ParkingLot parkingLot = new ParkingLot();
+
+            int ticket = parkingLot.park("paoche");
+
+            Assert.Same("paoche", parkingLot.pick(ticket));
+        }
+
+        [Fact]
+        public void should_be_able_to_pick_correct_car_when_custom_park_multi_diff_cars()
+        {
+            ParkingLot parkingLot = new ParkingLot();
+            int ticketA = parkingLot.park("Baoma");
+            int ticketB = parkingLot.park("luhu");
+            int ticketC = parkingLot.park("baoshijie");
+
+            Assert.Same("luhu", parkingLot.pick(ticketB));
+            Assert.Same("baoshijie",parkingLot.pick(ticketC));
+            Assert.Same("Baoma",parkingLot.pick(ticketA));
+
+        }
+
+        [Fact]
+        public void should_be_able_to_reject_parking_car_when_custome_park_car_to_full_parking_lot()
+        {
+
+        }
+
+
+    }
+}
